73cce284aca3cf95b6f8023b1d8ae902d373e2bc
SpaceTy <peselis20100@gmail.com>
2023-11-27 17:52:58 +0100

    Made it work

--- script.js
+++ script.js
@@ -1,57 +1,38 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const wordInput = document.getElementById('wordInput');
-    const randomWordsDiv = document.getElementById('randomWords');
-    randomWordsDiv.textContent = generateRandomWords(50);
+let particles = [];
+const colors = ['#FFD700', '#FFC700', '#FFB800'];
+const randomWordsDiv = document.getElementById('randomWords');
+    generateRandomWords(50)
     const canvas = document.getElementById('particleCanvas');
     const ctx = canvas.getContext('2d');
-    const cursor = document.getElementById('cursor');
-
-    let particles = [];
-    const colors = ['#FFD700', '#FFC700', '#FFB800'];
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    class Particle {
-        constructor(x, y) {
-            this.x = x;
-            this.y = y;
-            this.size = Math.random() * 5 + 1;
-            this.speedX = Math.random() * 3 - 1.5;
-            this.speedY = Math.random() * 3 - 1.5;
-            this.color = colors[Math.floor(Math.random() * colors.length)];
-        }
-
-        update() {
-            this.x += this.speedX;
-            this.y += this.speedY;
-            if (this.size > 0.2) this.size -= 0.1;
-        }
-
-        draw() {
-            ctx.fillStyle = this.color;
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-            ctx.fill();
-        }
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+        this.color = colors[Math.floor(Math.random() * colors.length)];
     }
 
-    function updateCursorPosition(input) {
-        const cursor = document.getElementById('cursor');
-        if (cursor) {
-            const textWidth = ctx.measureText(input).width;
-            cursor.style.left = `calc(50% - 50px + ${textWidth}px)`;
-        }
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.size > 0.2) this.size -= 0.1;
     }
 
-    wordInput.addEventListener('input', (event) => {
-        const inputText = event.target.value;
-        const targetText = randomWordsDiv.textContent.trim();
-    
-        if (targetText.startsWith(inputText)) {
-            updateCursorPosition(inputText);
-        }
-    });
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
 
     const addParticle = (x, y, count = 1) => {
         for (let i = 0; i < count; i++) {
@@ -86,16 +67,82 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     animate();
-
-    // Set the initial position of the cursor
-    updateCursorPosition(wordInput.value);
 });
 
+document.addEventListener('keydown', function(event) {
+    checkAndColorLetter(event.key)
+});
+
+function handleLetter(letter) {
+
+}
+
+function handleBackspace() {
+
+}
+
 function generateRandomWords(numberOfWords) {
-    const words = ["look", "one", "show", "develop", "world", /* ...other words... */];
+    const words = ["look", "one", "show", "develop", "world"];
     let randomWords = "";
     for (let i = 0; i < numberOfWords; i++) {
-        randomWords += words[Math.floor(Math.random() * words.length)] + " ";
+        breakWordIntoDivs(words[Math.floor(Math.random() * words.length)])
     }
-    return randomWords.trim(); // Trim the trailing space
+    
 }
+
+function breakWordIntoDivs(word) {
+    var wordDiv = document.createElement('div');
+    for (var i = 0; i < word.length; i++) {
+        var charDiv = document.createElement('div');
+        charDiv.textContent = word[i];
+        charDiv.className = 'letter';
+        wordDiv.appendChild(charDiv);
+    }
+
+    console.log("rtest")
+
+    const randomWordsDiv = document.getElementById("randomWords")
+    randomWordsDiv.appendChild(wordDiv);
+}
+
+function checkAndColorLetter(inputLetter) {
+    var randomWordsDiv = document.getElementById('randomWords');
+    if (randomWordsDiv.children.length === 0) {
+        return; // No words to check
+    }
+
+    var wordDiv = randomWordsDiv.children[0];
+    var letterDivs = wordDiv.children;
+    var allColored = true;
+
+    for (var i = 0; i < letterDivs.length; i++) {
+        var letterDiv = letterDivs[i];
+        if (!letterDiv.classList.contains('colored')) {
+            allColored = false; // There's at least one uncolored letter in the word
+
+            if (letterDiv.textContent === inputLetter) {
+                letterDiv.style.color = 'gold';
+                letterDiv.classList.add('colored');
+
+                // Create a particle explosion at the letter's position
+                const rect = letterDiv.getBoundingClientRect();
+                addParticle(rect.left + rect.width / 2, rect.top + rect.height / 2);
+
+                break; // Exit the loop after coloring a letter
+            }
+        }
+    }
+
+    if (allColored) {
+        randomWordsDiv.removeChild(wordDiv);
+    }
+}
+
+function addParticle(x, y, count = 10) {
+    for (let i = 0; i < count; i++) {
+        particles.push(new Particle(x, y));
+    }
+}
+
+
+
